251d61d1de9b97fff1dab4eb47c9d3be1ae7d85d
Swiftly01 <kareemkazeem100@gmail.com>
2025-12-06 00:27:08 +0100

    chore(utils): add dashboard data and credit officer data

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,55 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+interface MetricProps {
+  title: string;
+  value: string;
+  change: string;
+  changeColor: "green" | "red";
+  border: boolean;
+}
+
+export const creditOficcer: MetricProps[] = [
+  {
+    title: "Total Credit Officers",
+    value: "42,094",
+    change: "+6% this month",
+    changeColor: "green",
+    border: false,
+  },
+];
+
+export const data: MetricProps[] = [
+  {
+    title: "All Customers",
+    value: "42,094",
+    change: "+6% this month",
+    changeColor: "green",
+    border: false,
+  },
+  {
+    title: "All CO's",
+    value: "15,350",
+    change: "+6% this month",
+    changeColor: "green",
+    border: true,
+  },
+  {
+    title: "Loans Processed",
+    value: "28,350",
+    change: "-26% this month",
+    changeColor: "red",
+    border: true,
+  },
+  {
+    title: "Loan Amount",
+    value: "₦50,350.00",
+    change: "+40% this month",
+    changeColor: "green",
+    border: true,
+  },
+];
