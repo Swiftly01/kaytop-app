37e26f2e2992eca4307b3b78b0f66f2bd913145c
Swiftly01 <kareemkazeem100@gmail.com>
2025-12-10 17:17:01 +0100

    refactor(types): remove interface Routes from dashboard

--- app/types/dashboard.ts
+++ app/types/dashboard.ts
@@ -5,14 +5,3 @@ export interface MetricProps {
   changeColor: "green" | "red";
   border: boolean;
 }
-
-export interface Routes {
-  Bm: {
-    DASHBOARD: string;
-    CREDIT: string;
-    CUSTOMERS: string;
-    LOAN: string;
-    REPORT: string;
-    SETTING: string;
-  };
-}
