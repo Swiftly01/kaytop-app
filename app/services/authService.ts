f8f8c272df777881472393b9a5c1919185e3454a
Swiftly01 <kareemkazeem100@gmail.com>
2025-12-11 19:18:31 +0100

    feat(auth): integrate verify otp api endpoint

--- app/services/authService.ts
+++ app/services/authService.ts
@@ -1,6 +1,13 @@
 import { apiBaseUrl } from "@/lib/config";
 import axios from "axios";
-import { ForgotPasswordData, ForgotPasswordResponse, LoginData, LoginResponse } from "../types/auth";
+import {
+  ForgotPasswordData,
+  ForgotPasswordResponse,
+  LoginData,
+  LoginResponse,
+  OtpData,
+  VerifyOtpResponse,
+} from "../types/auth";
 
 export class AuthService {
   static async login(data: LoginData): Promise<LoginResponse> {
@@ -8,14 +15,33 @@ export class AuthService {
       `${apiBaseUrl}/auth/login`,
       data
     );
+    // console.log(response);
+    return response.data;
+  }
+
+  static async forgotPassword(
+    data: ForgotPasswordData
+  ): Promise<ForgotPasswordResponse> {
+    const response = await axios.post<ForgotPasswordResponse>(
+      `${apiBaseUrl}/auth/forgot-password`,
+      data
+    );
+    // console.log(response);
+    return response.data;
+  }
+
+  static async verifyOtp(data: OtpData): Promise<VerifyOtpResponse> {
+    const response = await axios.post<VerifyOtpResponse>(
+      `${apiBaseUrl}/otp/verify`,
+      data
+    );
    // console.log(response);
     return response.data;
   }
 
-  static async forgotPassword(data: ForgotPasswordData): Promise<ForgotPasswordResponse>{
-    const response = await axios.post<ForgotPasswordResponse>(`${apiBaseUrl}/auth/forgot-password`, data);
+  static async sendOtp(data: OtpData): Promise<VerifyOtpResponse> {
+    const response = await axios.post<VerifyOtpResponse>(`${apiBaseUrl}/otp/send`, data);
    // console.log(response);
     return response.data;
-
   }
 }
