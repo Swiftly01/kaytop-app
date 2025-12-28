a9fd0d3c310c41a41e11b633f8c2dc8a63bd6de1
Swiftly01 <kareemkazeem100@gmail.com>
2025-12-28 22:19:56 +0100

    feat(type): add customer data to types

--- app/types/customer.ts
+++ app/types/customer.ts
@@ -5,17 +5,63 @@ export interface CustomerMetrics {
 }
 
 export interface CustomerData {
+  accountStatus: string;
+  address: string;
   branch: string;
+  createdAt: string;
+  createdAtBy?: string | null;
+  dob: string;
   email: string;
   firstName: string;
+  guarantorAddress: string;
+  guarantorEmail: string;
+  guarantorName: string;
+  guarantorPhone: string;
+  guarantorPicture: string;
   id: number;
+  idNumber: string;
+  idPicture: string;
+  idType: string;
+  isVerified: boolean;
   lastName: string;
   mobileNumber: string;
-  profilePicture?: string | null;
+  profilePicture: string;
+  role: string;
   state: string;
+  updatedAt?: string | null;
+  verificationStatus: string;
+  verifiedAt: string;
 }
 
-export interface CustomerListResponse{
+export interface CustomerDataResponse {
+  data: CustomerData;
+}
+
+export interface CustomerListResponse {
   data: CustomerData[];
-  meta?: Meta
+  meta?: Meta;
+}
+
+export interface Transactions {
+  amount: string;
+  createdAt: string;
+  description: string;
+  id: number;
+  isApproved: boolean;
+  loanId?: string | null;
+  requiresManagerAuth: boolean;
+  savingsId: number;
+  type: string;
+}
+
+export interface CustomerSavingsData {
+  balance: string;
+  createdAt: string;
+  id: number;
+  transactions: Transactions[];
+}
+
+export interface CustomerSavingsResponse {
+  data: CustomerSavingsData;
+  meta?: Meta;
 }
