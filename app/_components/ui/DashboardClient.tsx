c7ae4a41e864e0361b913675d29f278f36b90e23
Swiftly01 <kareemkazeem100@gmail.com>
2025-12-28 13:05:46 +0100

    feat(ui): add laoading props to dashboardclient component

--- app/_components/ui/DashboardClient.tsx
+++ app/_components/ui/DashboardClient.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useDashboardQuery } from "@/app/dashboard/bm/queries/kpi/useDashboardQuery";
 import { useLoanDisbursedQuery } from "@/app/dashboard/bm/queries/loan/useLoanDisbursedQuery";
-import { useDashboardDateFilter } from "@/app/hooks/useDashboardDateFilter";
 import { getDashboardMetrics } from "@/lib/utils";
 import Metric from "./Metric";
 
@@ -10,6 +9,7 @@ import { useLoanRecollection } from "@/app/dashboard/bm/queries/loan/useLoanReco
 import { useMissedPayment } from "@/app/dashboard/bm/queries/loan/useMissedPayment";
 import { useSavings } from "@/app/dashboard/bm/queries/savings/useSavings";
 import { usePageChange } from "@/app/hooks/usePageChange";
+import { PaginationKey } from "@/app/types/dashboard";
 import DashboardHeader from "./DashboardHeader";
 import DisbursementLineChart from "./DisbursementLineChart";
 import DisbursementTable from "./table/DisbursementTable";
@@ -19,25 +19,49 @@ import SavingsTable from "./table/SavingsTable";
 
 export default function DashboardClient() {
   const { isLoading, error, data } = useDashboardQuery();
-  const { data: loan } = useLoanDisbursedQuery();
-  const { data: disburseVolume } = useDisburseVolume();
-  const { data: loanRecollection } = useLoanRecollection();
-  const { data: savings } = useSavings();
-  const { data: missedPayment } = useMissedPayment();
+  const {
+    isLoading: isLoadingLoan,
+    error: loanError,
+    data: loan,
+  } = useLoanDisbursedQuery();
+  const {
+    isLoading: isLoadingDisburse,
+    error: disburseError,
+    data: disburseVolume,
+  } = useDisburseVolume();
+  const {
+    isLoading: isLoadingLoanRecollection,
+    error: recollectionError,
+    data: loanRecollection,
+  } = useLoanRecollection();
+  const {
+    isLoading: isLoadingSavings,
+    error: savingsError,
+    data: savings,
+  } = useSavings();
+  const {
+    isLoading: isLoadingMissedPayment,
+    error: missedPaymentError,
+    data: missedPayment,
+  } = useMissedPayment();
   const { handlePageChange } = usePageChange();
 
   const { primary, secondary } = getDashboardMetrics({ data });
 
   return (
     <>
-      <DashboardHeader data={data} />
+      <DashboardHeader data={data} isLoading={isLoading} />
 
       <Metric item={primary} isLoading={isLoading} error={error} />
 
       <Metric item={secondary} cols={2} isLoading={isLoading} error={error} />
 
       <div className="flex flex-col p-5 my-5 bg-white h-[40vh]">
-        <DisbursementLineChart data={disburseVolume ?? []} />
+        <DisbursementLineChart
+          data={disburseVolume ?? []}
+          isLoading={isLoadingDisburse}
+          error={disburseError}
+        />
       </div>
       <div>
         <div className="my-5 text-gray-500 tabs tabs-border custom-tabs">
@@ -50,9 +74,13 @@ export default function DashboardClient() {
           />
           <div className="p-10 bg-white tab-content">
             <DisbursementTable
+              isLoading={isLoadingLoan}
+              error={loanError}
               item={loan?.data}
               meta={loan?.meta}
-              onPageChange={(page) => handlePageChange(page, "loanPage")}
+              onPageChange={(page) =>
+                handlePageChange(page, PaginationKey.loan_page)
+              }
             />
           </div>
 
@@ -64,10 +92,12 @@ export default function DashboardClient() {
           />
           <div className="p-10 bg-white tab-content">
             <RecollectionTable
+              isLoading={isLoadingLoanRecollection}
+              error={recollectionError}
               item={loanRecollection?.data}
               meta={loanRecollection?.meta}
               onPageChange={(page) =>
-                handlePageChange(page, "recollectionPage")
+                handlePageChange(page, PaginationKey.recollection_page)
               }
             />
           </div>
@@ -80,9 +110,13 @@ export default function DashboardClient() {
           />
           <div className="p-10 bg-white tab-content">
             <SavingsTable
+              isLoading={isLoadingSavings}
+              error={savingsError}
               item={savings?.data}
               meta={savings?.meta}
-              onPageChange={(page) => handlePageChange(page, "savingsPage")}
+              onPageChange={(page) =>
+                handlePageChange(page, PaginationKey.savings_page)
+              }
             />
           </div>
           <input
@@ -93,10 +127,12 @@ export default function DashboardClient() {
           />
           <div className="p-10 bg-white tab-content">
             <MissedPaymentTable
+              isLoading={isLoadingMissedPayment}
+              error={missedPaymentError}
               item={missedPayment?.data}
               meta={missedPayment?.meta}
               onPageChange={(page) =>
-                handlePageChange(page, "missedPaymentPage")
+                handlePageChange(page, PaginationKey.missed_payment_page)
               }
             />
           </div>
