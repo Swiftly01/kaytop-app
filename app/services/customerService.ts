98817b4c6e92ced84c3ae7685324cf5adabaa3cf
Swiftly01 <kareemkazeem100@gmail.com>
2025-12-28 22:19:22 +0100

    feat(service): integrate customaer loan endpoint

--- app/services/customerService.ts
+++ app/services/customerService.ts
@@ -1,7 +1,11 @@
 import apiClient from "@/lib/apiClient";
 import { apiBaseUrl } from "@/lib/config";
 import { AxiosError } from "axios";
-import { CustomerListResponse } from "../types/customer";
+import {
+  CustomerDataResponse,
+  CustomerListResponse,
+  CustomerSavingsResponse,
+} from "../types/customer";
 
 interface QueryParamsProps {
   customerId?: number;
@@ -10,7 +14,10 @@ interface QueryParamsProps {
 }
 
 export class CustomerService {
-  static async getCustomersByBranch({ page, limit }: QueryParamsProps): Promise<CustomerListResponse> {
+  static async getCustomersByBranch({
+    page,
+    limit,
+  }: QueryParamsProps): Promise<CustomerListResponse> {
     try {
       const response = await apiClient.get(`${apiBaseUrl}/users/my-branch`, {
         params: {
@@ -18,11 +25,66 @@ export class CustomerService {
           limit,
         },
       });
-     // console.log(response);
+      // console.log(response);
       return { data: response.data, meta: undefined };
     } catch (error: AxiosError | unknown) {
       const err = error as AxiosError;
       console.log("Error fetching all branch customer", err.response?.data);
+      throw err;
+    }
+  }
+
+  static async getBranchCustomerById(
+    customerId: number
+  ): Promise<CustomerDataResponse> {
+    try {
+      const response = await apiClient.get(
+        `${apiBaseUrl}/admin/users/${customerId}`
+      );
+      // console.log(response);
+      return { data: response.data };
+    } catch (error: AxiosError | unknown) {
+      const err = error as AxiosError;
+      console.log("Error fetching branch customer by id", err.response?.data);
+      throw err;
+    }
+  }
+
+  static async getBranchCustomerLoan(customerId: number) {
+    try {
+      const response = await apiClient.get(
+        `${apiBaseUrl}/loans/customer/${customerId}`
+      );
+      console.log(response);
+      return response.data;
+    } catch (error: AxiosError | unknown) {
+      const err = error as AxiosError;
+      console.log("Error fetching branch customer by id", err.response?.data);
+      throw err;
+    }
+  }
+
+  static async getBranchCustomerSavings({
+    customerId,
+    page,
+    limit,
+  }: QueryParamsProps): Promise<CustomerSavingsResponse> {
+    try {
+      const response = await apiClient.get(
+        `${apiBaseUrl}/savings/customer/${customerId}`,
+        {
+          params: {
+            customerId,
+            page,
+            limit,
+          },
+        }
+      );
+      // console.log(response);
+      return { data: response.data, meta: response.data.meta ?? null };
+    } catch (error: AxiosError | unknown) {
+      const err = error as AxiosError;
+      console.log("Error fetching branch customer by id", err.response?.data);
       throw err;
     }
   }
