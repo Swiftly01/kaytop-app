e049c8f6cf0b0afe74d4ce4459969d189a92b261
Swiftly01 <kareemkazeem100@gmail.com>
2025-12-31 10:50:13 +0100

    refactor(page): refactor  dashboard page

--- app/dashboard/bm/page.tsx
+++ app/dashboard/bm/page.tsx
@@ -1,15 +1,16 @@
 import DashboardClient from "@/app/_components/ui/DashboardClient";
-import SpinnerLg from "@/app/_components/ui/SpinnerLg";
+import { Spinner } from "@/components/ui/spinner";
 import { Suspense } from "react";
 
-export default function Page() {
+export default async function Page() {
+ 
   return (
     <div className="drawer-content">
       <div className="container h-full px-5 pt-4 mx-auto max-w-7xl">
         <Suspense
           fallback={
             <div className="flex items-center justify-center h-[70vh]">
-              <SpinnerLg />
+              <Spinner />
             </div>
           }
         >
