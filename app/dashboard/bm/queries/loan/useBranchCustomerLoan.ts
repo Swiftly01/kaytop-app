bdffae1d6ebcaf752e4955a888ba7f8728c9f7a2
Swiftly01 <kareemkazeem100@gmail.com>
2025-12-31 10:51:42 +0100

    refactor(hook): refactor useBranchCustomerLoan

--- app/dashboard/bm/queries/loan/useBranchCustomerLoan.ts
+++ app/dashboard/bm/queries/loan/useBranchCustomerLoan.ts
@@ -1,12 +1,18 @@
 import { CustomerService } from "@/app/services/customerService";
+import { ApiResponseError } from "@/app/types/auth";
+import { ActiveLoanData } from "@/app/types/loan";
 import { useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import { useParams } from "next/navigation";
 
 export function useBranchCustomerLoan() {
   const params = useParams();
   const customerId = Number(params.customerId);
 
-  const { isLoading, error, data } = useQuery({
+  const { isLoading, error, data } = useQuery<
+    ActiveLoanData[],
+    AxiosError<ApiResponseError>
+  >({
     queryKey: ["loan-customerId", customerId],
     queryFn: () => {
       return CustomerService.getBranchCustomerLoan(customerId);
