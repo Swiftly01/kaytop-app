25fa5365834ccdb5b94d96fa04c169a183d91402
Swiftly01 <kareemkazeem100@gmail.com>
2026-01-04 07:58:26 +0100

    chore(error): add error boundary handler

--- app/dashboard/bm/error.tsx
+++ app/dashboard/bm/error.tsx
@@ -1,14 +1,21 @@
 "use client";
 
-export default function Error({error, reset}) {
-  return (
-    <main className='flex flex-col items-center justify-center gap-6'>
-      <h1 className='text-3xl font-semibold'>Something went wrong!</h1>
-      <p className='text-lg'>{error.message}</p>
 
-      {/* <button className='inline-block px-6 py-3 text-lg bg-accent-500 text-primary-800' onClick={reset}>
+interface ErrorProps {
+  error: Error & { digest?: string };
+  reset: () => void;
+}
+
+export default function Error({ error, reset }: ErrorProps) {
+  return (
+    <main className="flex flex-col items-center justify-center gap-6">
+      <h1 className="text-3xl font-semibold">Something went wrong!</h1>
+
+      <p className="text-sm text-gray-500">{error.message}</p>
+
+      <button onClick={() => reset()} className="btn btn-primary">
         Try again
-      </button> */}
+      </button>
     </main>
   );
 }
