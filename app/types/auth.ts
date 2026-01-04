2892a8975e61787ac0c493d51ea66e98a268e0da
Swiftly01 <kareemkazeem100@gmail.com>
2026-01-04 06:49:23 +0100

    feat(types): add changePasswordData interface and nestValidationError Response type

--- app/types/auth.ts
+++ app/types/auth.ts
@@ -66,3 +66,15 @@ export interface ValidationErrorResponse {
 export interface ApiResponseError {
   message?: string;
 }
+
+export interface ChangePasswordData {
+  currentPassword: string;
+  newPassword: string;
+  confirmNewPassword: string;
+}
+
+export interface NestValidationErrorResponse {
+  statusCode: number;
+  message: string[];
+  error: string;
+}
