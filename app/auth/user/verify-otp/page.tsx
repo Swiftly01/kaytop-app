86cb03d2352732df298efb966d582503d638a55a
Faree99 <adebayofaridah99@gmail.com>
2026-01-15 23:58:11 +0100

    updated

--- app/auth/user/verify-otp/page.tsx
+++ app/auth/user/verify-otp/page.tsx
@@ -1,25 +1,20 @@
-"use client"
+import { Spinner } from "@/components/ui/spinner";
+import { Suspense } from "react";
+import VerifyOtpClient from "./VerifyOtpClient";
 
-import VerifyOtpForm from "@/app/_components/ui/auth/user/VerifyOtpForm";
-import { Purpose } from "@/app/types/auth";
-
-// export const metadata = {
-//   title: "Verify OTP"
+export const metadata = {
+  title: "Verify OTP"
   
-// }
-export default function page() {
-  
-  return (
-        <div className="bg-white rounded-xl shadow-md p-10 w-full mx-auto max-w-md md:max-w-xl mt-6">
-      <h1 className="text-3xl font-medium text-neutral-700">
-        Verify your email
-      </h1>
-      <p className="text-neutral-700 text-md">
-        Please enter the OTP sent to your email for verification
-      </p>
+}
 
-      <VerifyOtpForm />
-     
-    </div>
-  );
+export default function page() { 
+ <Suspense
+          fallback={
+            <div className="flex items-center justify-center h-[70vh]">
+              <Spinner />
+            </div>
+          }
+        >
+          <VerifyOtpClient />
+        </Suspense>
 }
